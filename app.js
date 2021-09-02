06c1a67d1a562fd9f7e2772f597d1d131cfbb0d8
Kandpal, Harshit <hkandpal@cvent.com>
2021-09-02 13:05:50 +0530

    Added alternate mode to allow conversion to JSON irrespective of column headers

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const fs = require('fs');
 const xlsx = require('node-xlsx');
 
 const filePath = process.argv[2];
-const sheetName = process.argv[3]; 
+const sheetName = process.argv[3];
+const mode = process.argv[4]; 
 
 const getWorksheet = (path, sheetName) => {
     try {
@@ -25,35 +26,52 @@ const getWorksheet = (path, sheetName) => {
     }
 };
 
-const buildJSON = (filePath, sheetName) => {
+const buildJSON = (filePath, sheetName, mode = 1) => {
     try {
         const data = getWorksheet(filePath, sheetName);
 
-        const jsonArray  = [];
-
-        const entityIdIndex = data[0].findIndex(el => el === 'Venue ID');
-        const accountIdIndex = data[0].findIndex(el => el === 'Account ID');
-        const localeIndex = data[0].findIndex(el => el === 'Locale');
-
-        if (entityIdIndex === -1 || accountIdIndex === -1 || localeIndex === -1) {
-            throw `Invalid column name(s). Sheet needs to have "Venue ID", "Account ID" and "Locale" column names.`;
-        }
+        const jsonArray = [];
 
         const iterationDataset = data.slice(1,data.length);
 
-        for (const record of iterationDataset) {
-            jsonArray.push({
-                entityId: record[entityIdIndex],
-                accountId: record[accountIdIndex],
-                localeIndex: record[localeIndex]
-            })
-        }
+        if (mode === '1') {
+            const keyArray = data[0];
 
-        fs.writeFileSync('jsonArray.json', JSON.stringify(jsonArray));
+            for (const record of iterationDataset) {
+                let currentObject = {};
+
+                keyArray.forEach((el, index) => {
+                    currentObject[el] = record[index];
+                });
+
+                jsonArray.push(currentObject);
+            }
+
+            fs.writeFileSync('jsonArrayMode1.json', JSON.stringify(jsonArray))
+            
+        } else {
+            const entityIdIndex = data[0].findIndex(el => el === 'Venue ID');
+            const accountIdIndex = data[0].findIndex(el => el === 'Account ID');
+            const localeIndex = data[0].findIndex(el => el === 'Locale');
+
+            if (entityIdIndex === -1 || accountIdIndex === -1 || localeIndex === -1) {
+                throw `Invalid column name(s). Sheet needs to have "Venue ID", "Account ID" and "Locale" column names.`;
+            }
+
+            for (const record of iterationDataset) {
+                jsonArray.push({
+                    entityId: record[entityIdIndex],
+                    accountId: record[accountIdIndex],
+                    localeIndex: record[localeIndex]
+                })
+            }
+
+            fs.writeFileSync('jsonArrayMode2.json', JSON.stringify(jsonArray));
+        }
 
     } catch(err) {
         console.error(err);
     }
 }
 
-buildJSON(filePath, sheetName);
+buildJSON(filePath, sheetName, mode);
